f7c7475ab29fd06c8b1a1cedfab6ae7147fd5073
Kobermann151 <Kobercosta@proton.me>
2026-07-30 11:20:54 -0300

    added code

---
+++ src/sec.java
@@ -0,0 +1,16 @@
+import java.util.List;
+import java.util.Scanner;
+
+public class sec {
+    public static void main(String[] args) {
+        System.out.println("bem vindo ao site do banco genérico");
+    }
+
+    boolean possuiConta; // true
+    int nome = "joao";
+    int cpf = "1234567890";
+    int idade = "52";
+    int saldo = "10000";
+    int data = "11/11/2011";
+
+}
