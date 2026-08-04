8d0ea285231ed9c6ddf111e26ee15ddf383be169
Kobermann151 <Kobercosta@proton.me>
2026-08-04 10:11:06 -0300

    added code

--- src/sec.java
+++ src/sec.java
@@ -1,16 +1,19 @@
-import java.util.List;
 import java.util.Scanner;
 
 public class sec {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ScamDetector detector = new ScamDetector();
+
         System.out.println("bem vindo ao site do banco genérico");
+        System.out.println("Digite aqui a Mensagem para detecção");
+
+        while (true) {
+            System.out.print("\nMensagem: ");
+            String input = scanner.nextLine();
+
+            if (input.equalsIgnoreCase("sair"));
+                break;
+            }
     }
-
-    boolean possuiConta; // true
-    int nome = "joao";
-    int cpf = "1234567890";
-    int idade = "52";
-    int saldo = "10000";
-    int data = "11/11/2011";
-
 }
